d2f08a30c52173764068171842da9ef5f75d5e9f
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2013-05-02 09:25:35 +0200

    Initial Commit

---
+++ HomePwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HomePwner
+//
+//  Created by Thomas Eng on 02.05.13.
+//  Copyright (c) 2013 Thomas Eng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomePwnerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomePwnerAppDelegate class]));
+    }
+}
