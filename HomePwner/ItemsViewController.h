ff860aac08570ee9b1334291ca11a10aacb8052d
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2013-05-02 11:33:02 +0200

    Added.

---
+++ HomePwner/ItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.h
+//  HomePwner
+//
+//  Created by Thomas Eng on 02.05.13.
+//  Copyright (c) 2013 Thomas Eng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemsViewController : UITableViewController
+
+@end
---
+++ HomePwner/ItemsViewController.m
@@ -0,0 +1,26 @@
+//
+//  ItemsViewController.m
+//  HomePwner
+//
+//  Created by Thomas Eng on 02.05.13.
+//  Copyright (c) 2013 Thomas Eng. All rights reserved.
+//
+
+#import "ItemsViewController.h"
+
+@implementation ItemsViewController
+
+- (id)init
+{
+    self = [super initWithStyle:UITableViewStyleGrouped];
+    if(self) {
+    }
+    return self;
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+@end
